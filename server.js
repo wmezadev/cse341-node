8183799c159452fc73eee99b0e07359dce447311
William Eduardo Meza <williameduardomeza@gmail.com>
2022-11-02 23:57:14 -0500

    feat: creates first routes structure file

--- server.js
+++ server.js
@@ -2,9 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('William Meza!')
-})
+app.get('/', require('./routes'))
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
