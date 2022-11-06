99caaf9f5f12049e84105fc8d94a9064f1f68f9a
William Eduardo Meza <williameduardomeza@gmail.com>
2022-11-06 00:13:30 -0500

    add MongoDB contection

---
+++ db/index.js
@@ -0,0 +1,24 @@
+const { MongoClient } = require("mongodb");
+const dotenv = require('dotenv');
+dotenv.config();
+let dbConnection;
+
+module.exports = {
+  initDb: function (callback) {
+    if (dbConnection) {
+      return callback(null, dbConnection);
+    }
+    MongoClient.connect(process.env.MONGODB_URI)
+      .then((client) => {
+        dbConnection = client;
+        callback(null, dbConnection);
+      })
+      .catch((err) => {
+        callback(err);
+      });
+  },
+
+  getDb: () => {
+    return dbConnection;
+  },
+};
--- server.js
+++ server.js
@@ -1,9 +1,25 @@
-const express = require('express')
-const app = express()
-const port =process.env.PORT || 3000;
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongodb = require('./db');
+const dotenv = require('dotenv');
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 3000;
 
-app.get('/', require('./routes'))
+app
+  .use(cors())
+  .use(bodyParser.json())
+  .use(require('./routes'))
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+mongodb.initDb((err, mongodb) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(`Connected to DB and listening on ${port}`);
+  }
+});
