73a4e65a5906796f10776409025df1687ef3026a
William Eduardo Meza <williameduardomeza@gmail.com>
2022-11-10 18:43:09 -0500

    feat: add PUT method for contacts

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -4,7 +4,6 @@ const ObjectId = require('mongodb').ObjectId;
 const index = async (req, res) => {
   const result = await mongodb.getDb().db().collection('contacts').find();
   result.toArray().then((lists) => {
-    res.setHeader('Content-Type', 'application/json');
     res.status(200).json(lists);
   });
 };
@@ -13,20 +12,39 @@ const show = async (req, res) => {
   const userId = new ObjectId(req.params.id);
   const result = await mongodb.getDb().db().collection('contacts').find({ _id: userId });
   result.toArray().then((lists) => {
-    res.setHeader('Content-Type', 'application/json');
     res.status(200).json(lists[0]);
   });
 };
 
 const store = async (req, res) => {
-  const { firstName, lastName, email, favoriteColor, birthday } = req.body;
-  const contact = { firstName, lastName, email, favoriteColor, birthday };
   try {
+    const { firstName, lastName, email, favoriteColor, birthday } = req.body;
+    const contact = { firstName, lastName, email, favoriteColor, birthday };
     const response = await mongodb.getDb().db().collection('contacts').insertOne(contact);
     res.status(201).json(response);
-  } catch (error) {
-    res.status(500).json(error);
+  } catch (err) {
+    // TODO: add a Log handler feature instead of using console.error(error);
+    res.status(500).json(err);
   }
 };
 
-module.exports = { index, show, store };
+const update = async (req, res) => {
+  try {
+    const userId = new ObjectId(req.params.id);
+    const { firstName, lastName, email, favoriteColor, birthday } = req.body;
+    const contact = { firstName, lastName, email, favoriteColor, birthday };
+    const response = await mongodb
+      .getDb()
+      .db()
+      .collection('contacts')
+      .replaceOne({ _id: userId }, contact);
+    if (!(response.modifiedCount > 0)) {
+      throw new Error('Error while updating contact.');
+    }
+    res.status(204).send();
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+module.exports = { index, show, store, update };
--- routes/contacts.js
+++ routes/contacts.js
@@ -4,5 +4,6 @@ const ContactController = require('../controllers/contacts');
 router.get('/', ContactController.index);
 router.get('/:id', ContactController.show);
 router.post('/', ContactController.store);
+router.put('/:id', ContactController.update);
 
 module.exports = router;
